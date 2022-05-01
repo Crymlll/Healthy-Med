33366a114b42de338b866b0116122646255359fb
nadeeee <aurigaf@gmail.com>
2022-05-01 22:43:21 +0700

    Front-end
    Login,signin,Homepage bang sisanya belum. navbar masi 1 state pas login doang

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -1,4 +1,55 @@
-<!DOCTYPE html>
+
+@extends('layouts.signin')
+@section('title')
+@endsection
+@section('content')
+    <div class="container">
+      <div class="row align-items-center justify-content-center vh-100">
+        <div class="col-md-7">
+          <img src="{{asset('storage/images/HealthyMed.png')}}" class="w-50">
+          <h3>Before get we get started let’s create you’re account.</h3>
+        </div>
+        <div class="col-md-5">
+          <form class="needs-validation" action="/register" method="post">
+            @csrf
+
+            <div class="mb-3">
+                <input type="text" class="form-control" for="name" placeholder="Name" name="name" id="name" 
+                required>
+              </div>
+
+            <div class="mb-3">
+              <input type="text" class="form-control" for="username" placeholder="Email address or phone number" name="email" id="email"
+                required>
+            </div>
+            <div class="mb-3">
+              <input type="password" class="form-control" for="password" placeholder="Password" name="password" id="password"
+                required>
+            </div>
+            <button type="submit" class="btn btn-custom btn-lg btn-block mt-3">Create Account</button>
+            <div class="text-center pt-3 pb-3">
+              <a href="#" class="">Forgotten password?</a>
+              <hr>
+              <p> Already have an account ? <br> <button class="ajx" type="button">Signin here</button> </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div> 
+
+@section('script')
+<script>
+    $(document).ready(function(){
+        $("button").click(function(){
+            $(".container").load("/login");
+        });
+    });
+    </script>
+@endsection
+
+
+
+{{-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -29,4 +80,4 @@
         <input type="submit">
     </form>
 </body>
-</html>
+</html> --}}
