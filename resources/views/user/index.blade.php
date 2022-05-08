4fbf49b05de13e3d98c8ab8bbc5dadcadbc4def0
nadeeee <aurigaf@gmail.com>
2022-05-09 03:15:38 +0700

    Front-End 2
    banyak deh maseh dari rapihin sampe buat

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,51 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300i,400,700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="scss/homepage.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="{{ URL::asset('js/jquery-3.6.0.min.js') }}"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-</head> 
-
-<nav class="navbar navbar-expand-xl navbar-dark bg-dark">
-	<a href="#" class="navbar-brand"><i class="fa fa-med"></i>Healthy<b>Med</b></a>  		
-	<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-	<!-- Collection of nav links, forms, and other content for toggling -->
-	<div id="navbarCollapse" class="collapse navbar-collapse justify-content-start">		
-		<form class="navbar-form form-inline">
-			<div class="input-group search-box">								
-				<input type="text" id="search" class="form-control" placeholder="Search here...">
-				<span class="input-group-addon"><i class="material-icons">&#xE8B6;</i></span>
-			</div>
-		</form>
-		<div class="navbar-nav ml-auto">
-			<a href="/" class="nav-item nav-link active"><i class="material-icons">&#xe88a;</i><span>Home</span></a>
-			<a href="/liked" class="nav-item nav-link"><i class="material-icons">&#xe87d;</i><span>Liked</span></a>	
-			<div class="nav-item dropdown">
-				<a href="#" data-toggle="dropdown" class="nav-item nav-link dropdown-toggle user-action"><img src="{{asset('storage/images/img.jpg')}}"  class="avatar" alt="Avatar"><b> {{auth()->user()->name}} <b class="caret"></b></a>
-				<div class="dropdown-menu">
-					<a href="#" class="dropdown-item"><i class="fa fa-user-o"></i> Profile</a>
-					<a href="#" class="dropdown-item"><i class="fa fa-sliders"></i> Settings</a>
-					<div class="divider dropdown-divider"></div>
-                    <form action="/logout" method="post">
-                        @csrf
-                        {{-- <button class="logout" type="submit"><i class="material-icons">&#xE8AC;</i>logout</button> --}}
-                         <button class="dropdown-item" type="submit"><i class="material-icons">&#xE8AC;</i> Logout</button>
-                    </form>
-				</div>
-			</div>
-		</div>
-	</div>
-</nav>
+@extends('layouts.navbar')
+@section('navbar')
+<a href="/" class="nav-item nav-link"><i class="material-icons">&#xe88a;</i><span>Home</span></a>
+<a href="/liked" class="nav-item nav-link"><i class="material-icons">&#xe87d;</i><span>Liked</span></a>	
+<a href="/my_article" class="nav-item nav-link active"><i class="material-icons">&#xe80b;</i><span>My Article</span></a>
+@endsection
+@section('content')
 
 <body>
 <div class="container">
@@ -130,14 +89,18 @@
 					selengkapnya.href = '/article/id/'+element.id
 					berita_box.appendChild(selengkapnya)
 
-					var isi = document.createElement('p')
-					isi.innerHTML = element.isi
-					berita_box.appendChild(isi)
+					var br = document.createElement('br')
+					berita_box.appendChild(br)
 
 					var like = document.createElement('a')
-					like.innerHTML = element.total_like
+					like.innerHTML = '<i onclick="myFunction(this)" class="fa fa-heart-o"></i> ' + element.total_like
 					like.href = '/like/'+element.id
 					berita_box.appendChild(like)
+
+					var keterangan = document.createElement('div')
+					keterangan.className = 'keterangan'
+					keterangan.innerHTML = 'Waktu : ' + element.created_at
+					berita_box.appendChild(keterangan)
 
 					beritaList.appendChild(berita_box)
 				});
@@ -148,3 +111,4 @@
     </script>
 </body>
 </html>
+@endsection
